7b31749ba5454e6ae734dbffc672f853e5995629
Shota Kishimoto <shokishimo@gmail.com>
2022-12-04 18:16:07 -0600

    database and docker setting

---
+++ databae/migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE QuizData
+(
+    ID bigint auto_increment,
+    Question varchar(255) NOT NULL,
+    Answer varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO QuizData(Question, Answer) VALUES ('low-key', '「控え目、地味」「秘密」「ちょっと、なんとなく」'), ('sloppy', '雑な');
